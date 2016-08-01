4c90d1d2d3f5bf2177a611fe3e6a3b3ecddd2cdb
Sean Wines <seanwinesdev@gmail.com>
2016-08-01 16:19:14 -0700

    Working commit

--- google_clone/js/script.js
+++ google_clone/js/script.js
@@ -2,31 +2,31 @@ var windowWidth = $(window).width();
 
 $(document).ready(function() {
 
-// Show th Tooltip
-  $('.th').on('click', function() {
-    $('.thToolTip').show();
-  })
-
-// Show bell Tooltip on desktop / app link on mobile
-  if (windowWidth <= 480) {
-    $('.bell-link').attr('href', 'https://notifications.google.com/widget/mobile');
-  } else {
-    $('.bell').on('click', function() {
-      $('.bellToolTip').show();
+    // Show th Tooltip
+    $('.th').on('click', function() {
+        $('.thToolTip').show();
     })
-  }
 
-// Show blue outline around search bar
-  $('.search input').on('click', function(){
-      $('.search').addClass('search-border');
-      console.log(showBoarder);
-  })
+    // Show bell Tooltip on desktop / app link on mobile
+    if (windowWidth <= 480) {
+        $('.bell-link').attr('href', 'https://notifications.google.com/widget/mobile');
+    } else {
+        $('.bell').on('click', function() {
+            $('.bellToolTip').show();
+        })
+    }
 
-// Remove search bar outline & tooltips when body is clicked
-  $("body").mouseup(function(){
-    $('.search').removeClass('search-border');
-    $('.thToolTip').hide();
-    $('.bellToolTip').hide();
-  });
+    // Show blue outline around search bar
+    $('.search input').on('click', function() {
+        $('.search').addClass('search-border');
+        console.log(showBoarder);
+    })
+
+    // Remove search bar outline & tooltips when body is clicked
+    $("body").mouseup(function() {
+        $('.search').removeClass('search-border');
+        $('.thToolTip').hide();
+        $('.bellToolTip').hide();
+    });
 
 });
