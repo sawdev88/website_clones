50414e5c6df9bcf14aec22f09e66b2f1fea179c3
Sean Wines <seanwinesdev@gmail.com>
2016-08-01 15:42:16 -0700

    Added search outline show/hide

--- google_clone/js/script.js
+++ google_clone/js/script.js
@@ -1,46 +1,16 @@
+var showBoarder = false;
 
-var
-    show = true,
-    thButton = document.querySelector('.th'),
-    thToolTip = document.querySelector('.thToolTip'),
-    bellButton = document.querySelector('.bell'),
-    bellToolTip = document.querySelector('.bellToolTip');
+$(document).ready(function() {
+// Show blue outline around search bar
+  $('.search input').on('click', function(){
+      showBoarder = false;
+      $('.search').addClass('search-border');
+      console.log(showBoarder);
+  })
 
-thButton.addEventListener('click', function () {
+// Remove search bar outline when body is clicked
+  $("body").mouseup(function(){
+    $('.search').removeClass('search-border');
+  });
 
-
-
-  if (show) {
-    bellToolTip.style.display = 'none';
-    thToolTip.style.display = 'block';
-    show = false;
-  } else {
-    thToolTip.style.display = 'none';
-    show = true;
-  }
-
-})
-
-bellButton.addEventListener('click', function () {
-
-  show = true;
-
-  if (show) {
-    thToolTip.style.display = 'none';
-    bellToolTip.style.display = 'block';
-    show = false;
-  } else {
-    bellToolTip.style.display = 'none';
-    show = true;
-  }
-
-})
-
-window.addEventListener('click', function () {
-
-  if (show){
-    thToolTip.style.display = 'none';
-    bellToolTip.style.display = 'none';
-  }
-
-})
+});
