1a0af178a3f8e13850b4a2a69e238b694b73614c
Sean Wines <seanwinesdev@gmail.com>
2016-08-01 16:03:22 -0700

    jQuery Added

--- google_clone/js/script.js
+++ google_clone/js/script.js
@@ -1,6 +1,22 @@
-var showBoarder = false;
+var showBoarder = false,
+    windowWidth = $(window).width();
 
 $(document).ready(function() {
+
+// Show th Tooltip
+  $('.th').on('click', function() {
+    $('.thToolTip').show();
+  })
+
+// Show bell Tooltip
+  if (windowWidth <= 480) {
+    $('.bell-link').attr('href', 'https://notifications.google.com/widget/mobile');
+  } else {
+    $('.bell').on('click', function() {
+      $('.bellToolTip').show();
+    })
+  }
+
 // Show blue outline around search bar
   $('.search input').on('click', function(){
       showBoarder = false;
@@ -11,6 +27,8 @@ $(document).ready(function() {
 // Remove search bar outline when body is clicked
   $("body").mouseup(function(){
     $('.search').removeClass('search-border');
+    $('.thToolTip').hide();
+    $('.bellToolTip').hide();
   });
 
 });
